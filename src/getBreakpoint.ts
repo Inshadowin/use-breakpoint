ac3d5c31f58d9c45d397f8f47d351b6175b9d904
Antony <antony.karachansky@spsoft.com>
2023-03-06 23:18:36 +0200

    simplified algo

--- src/getBreakpoint.ts
+++ src/getBreakpoint.ts
@@ -6,15 +6,15 @@ export const getBreakpoint = <T extends string>(
   breakpointsMap: BreakpointsMapType<T> = defaultBreakpointsMap as BreakpointsMapType<T>
 ): T => {
   let resPoint: T = undefined;
-  let resWidth = undefined;
 
   Object.entries(breakpointsMap).forEach(([point, pointWidth]: [T, number]) => {
+    const resWidth = breakpointsMap[resPoint];
+
     if (
-      resWidth === undefined ||
+      resPoint === undefined ||
       (width >= pointWidth && resWidth < pointWidth) ||
       (width < pointWidth && resWidth >= pointWidth)
     ) {
-      resWidth = pointWidth;
       resPoint = point;
     }
   });
