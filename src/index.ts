931ccec6ab5812b9215adf67fa8454271070b6b1
Antony <antony.karachansky@spsoft.com>
2023-03-07 08:43:26 +0200

    useBreakpointFits added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { useBreakpoint } from './useBreakpoint';
-export { useFitsBreakpoint } from './useBreakpoint';
+export { useBreakpointFits } from './useBreakpoint';
 export { defaultBreakpointsMap } from './compareBreakpoints';
 export type { Breakpoint, BreakpointsMapType } from './types';
