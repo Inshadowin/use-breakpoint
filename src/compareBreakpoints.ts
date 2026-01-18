16b1a2572ff888dcfe9cd447037df6442e184f71
Inshadow <playersmail2@gmail.com>
2026-01-18 13:59:35 +0200

    fixed TS types

--- src/compareBreakpoints.ts
+++ src/compareBreakpoints.ts
@@ -1,6 +1,6 @@
 import type { Breakpoint, BreakpointsMapType } from './types';
 
-export const defaultBreakpointsMap: BreakpointsMapType<any> = {
+export const defaultBreakpointsMap: BreakpointsMapType<Breakpoint> = {
   xs: 0,
   sm: 640,
   md: 768,
@@ -14,7 +14,7 @@ export const defaultBreakpointsMap: BreakpointsMapType<any> = {
 export const compareBreakpoints = <T extends string = Breakpoint>(
   compare: T,
   current?: T,
-  breakpointsMap = defaultBreakpointsMap
+  breakpointsMap = defaultBreakpointsMap as BreakpointsMapType<T>
 ) => {
   const currentSize = breakpointsMap[current] ?? 0;
   const compareSize = breakpointsMap[compare] ?? 0;
--- src/useBreakpoint.ts
+++ src/useBreakpoint.ts
@@ -8,17 +8,17 @@ import {
 
 import { getBreakpoint } from './getBreakpoint';
 import { compareBreakpoints } from './compareBreakpoints';
-import { defaultBreakpointsMap } from './compareBreakpoints';
+import { defaultBreakpointsMap as map } from './compareBreakpoints';
 import type { Breakpoint, BreakpointsMapType } from './types';
 
-export const useCurrentBreakpoint = <T extends string = Breakpoint>(
+export const useCurrentBreakpoint = <T extends Breakpoint = Breakpoint>(
   breakpointsMap?: BreakpointsMapType<T>
 ): T => {
   const [, startTransition] = useTransition();
   const checkBreakpointRef = useRef(() => {
-    return getBreakpoint(window.innerWidth, breakpointsMap);
+    return getBreakpoint<T>(window.innerWidth, breakpointsMap);
   });
-  const [breakpoint, setBreakpoint] = useState(checkBreakpointRef.current);
+  const [breakpoint, setBreakpoint] = useState<T>(checkBreakpointRef.current);
 
   useLayoutEffect(() => {
     const handleCheck = () => {
@@ -36,30 +36,28 @@ export const useCurrentBreakpoint = <T extends string = Breakpoint>(
   return breakpoint;
 };
 
-export const useBreakpoint = <T extends string = Breakpoint>(
+export const useBreakpoint = <T extends Breakpoint = Breakpoint>(
   point: T,
   breakpointsMap?: BreakpointsMapType<T>
 ) => {
-  const currentBp = useCurrentBreakpoint<T>(
-    (defaultBreakpointsMap as BreakpointsMapType<T>) ?? breakpointsMap
-  );
+  const currentBp = useCurrentBreakpoint<T>(breakpointsMap ?? map);
 
   return compareBreakpoints<T>(point, currentBp, breakpointsMap);
 };
 
-export const useBreakpointFits = <T extends string = Breakpoint>(
+export const useBreakpointFits = <T extends Breakpoint = Breakpoint>(
   point: T,
   breakpointsMap?: BreakpointsMapType<T>
 ) => {
-  return useBreakpoint(point, breakpointsMap) === 'fits';
+  return useBreakpoint<T>(point, breakpointsMap) === 'fits';
 };
 
-export const useBreakpointChecker = <T extends string = Breakpoint>(
+export const useBreakpointChecker = <T extends Breakpoint = Breakpoint>(
   breakpointsMap?: BreakpointsMapType<T>
-) => {
+): { [x in T]: boolean } => {
   const current = useCurrentBreakpoint<T>(breakpointsMap);
   const checker = useMemo(() => {
-    return new Proxy<{ [x in T]: boolean }>({} as any, {
+    return new Proxy<{ [x in T]: boolean }>({} as { [x in T]: boolean }, {
       get: (_target, key: T) => compareBreakpoints(key, current) === 'fits',
     });
   }, [current]);
